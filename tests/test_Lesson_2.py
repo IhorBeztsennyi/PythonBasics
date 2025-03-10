0cb4d5060dc8bf68c7465fc225bf31fdff239b14
ihor_beztsennyi <ihor_beztsennyi@epam.com>
2025-03-10 14:03:56 +0200

    @pytest.mark.parametrize to test_Lesson_2.py added

--- tests/test_Lesson_2.py
+++ tests/test_Lesson_2.py
@@ -15,58 +15,31 @@ def test_generate_random_dicts():
             assert 0 <= value <= 100
 
 
-def test_aggregate_dicts():
-    dict_list = [
-        {'a': 10, 'b': 20},
-        {'a': 15, 'c': 30},
-        {'b': 25, 'd': 40}
-    ]
+@pytest.mark.parametrize("dict_list, expected_result", [
+    (
+        [{'a': 10, 'b': 20}, {'a': 15, 'c': 30}, {'b': 25, 'd': 40}],
+        {'a_2': 15, 'b_3': 25, 'c': 30, 'd': 40}
+    ),
+    (
+        [{'x': 5}, {'x': 10}, {'y': 20}],
+        {'x_2': 10, 'y': 20}
+    ),
+    (
+        [],
+        {}
+    ),
+    (
+        [{}, {}, {}, {}],
+        {}
+    ),
+    (
+        [{'a': 10, 'b': 20}, {'a': 10, 'b': 20}, {'a': 10, 'b': 20}],
+        {'a_1': 10, 'b_1': 20}
+    )
+])
+def test_aggregate_dicts(dict_list, expected_result):
     result_dict = aggregate_dicts(dict_list)
-    expected_result = {
-        'a_2': 15,
-        'b_3': 25,
-        'c': 30,
-        'd': 40
-    }
     assert result_dict == expected_result
-
-    dict_list = [
-        {'x': 5},
-        {'x': 10},
-        {'y': 20}
-    ]
-    result_dict = aggregate_dicts(dict_list)
-    expected_result = {
-        'x_2': 10,
-        'y': 20
-    }
-    assert result_dict == expected_result
-
-
-# Test aggregate_dicts function when the list is empty
-def test_aggregate_dicts_empty_list():
-    dict_list = []
-    result_dict = aggregate_dicts(dict_list)
-    expected_result = {}
-    assert result_dict == expected_result
-
-# Test all dictionaries are empty
-def test_aggregate_dicts_all_empty_dicts():
-    dict_list = [{}, {}, {}, {}]
-    result_dict = aggregate_dicts(dict_list)
-    expected_result = {}
-    assert result_dict == expected_result
-
-# Test all dictionaries are the same
-def test_aggregate_dicts_all_same_dicts():
-    dict_list = [{'a': 10, 'b': 20}, {'a': 10, 'b': 20}, {'a': 10, 'b': 20}]
-    result_dict = aggregate_dicts(dict_list)
-    expected_result = {
-        'a_1': 10,
-        'b_1': 20
-    }
-    assert result_dict == expected_result
-
 
 if __name__ == "__main__":
     pytest.main()
